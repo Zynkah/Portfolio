f5be45e6740034f6cd2be282152501780f150f20
Zynkah <ffgurl07@yahoo.com>
2023-06-05 13:32:05 -0500

    Card Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { Card, CardActionArea, CardMedia } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
 import Grid from "@mui/material/Grid";
 
 function App() {
@@ -29,26 +37,60 @@ function App() {
                     />
                   </a>
                 </CardActionArea>
+                <CardContent>
+                  <Typography>
+                    Final Fantasy X Fan Page built with Next.js, Next UI and
+                    Material UI.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <a
+                    href="https://github.com/Zynkah/FFX_Next.js"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button size="small" color="primary">
+                      GitHub Source Code
+                    </Button>
+                  </a>
+                </CardActions>
               </Card>
             </Grid>
 
             <Grid item xs={6}>
               <Card>
                 <CardActionArea>
-                <a
-                  className="App-link"
-                  href="https://maid-cafe.vercel.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                  <a
+                    className="App-link"
+                    href="https://maid-cafe.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <CardMedia
                       component="img"
                       height="400"
                       image="/img/Cafe.png"
                       alt="Final fantasy x"
                     />
-                </a>
+                  </a>
                 </CardActionArea>
+                <CardContent>
+                  <Typography>
+                    Mock Maid Cafe Website built with React.js, Node.js and
+                    Bootstrap.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <a
+                    href="https://github.com/Zynkah/React_Maid_Cafe"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button size="small" color="primary">
+                      GitHub Source Code
+                    </Button>
+                  </a>
+                </CardActions>
               </Card>
             </Grid>
 
