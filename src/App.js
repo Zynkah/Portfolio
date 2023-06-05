7c3e6de71f9e0fe432bda7913099cfbec3229223
Zynkah <ffgurl07@yahoo.com>
2023-06-05 12:48:09 -0500

    Cards Added

--- src/App.js
+++ src/App.js
@@ -1,44 +1,84 @@
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Card, CardActionArea, CardMedia } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Welcome to my portolio!
-        </p>
-        <a
-          className="App-link"
-          href="https://final-fantasy-x.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Final Fantasy X Fan Page
-        </a>
-        <a
-          className="App-link"
-          href="https://maid-cafe.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Zynkah's Maid Cafe
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/Zynkah"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My GitHub
-        </a>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/zena-creps-ba3631232/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
+        <p>Welcome to my portolio!</p>
+
+        <Box sx={{ flexGrow: 1, margin: "2rem" }}>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <Card>
+                <CardActionArea>
+                  <a
+                    className="App-link"
+                    href="https://final-fantasy-x.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <CardMedia
+                      component="img"
+                      height="400"
+                      image="/img/FFX.png"
+                      alt="Final fantasy x"
+                    />
+                  </a>
+                </CardActionArea>
+              </Card>
+            </Grid>
+
+            <Grid item xs={6}>
+              <Card>
+                <CardActionArea>
+                <a
+                  className="App-link"
+                  href="https://maid-cafe.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                    <CardMedia
+                      component="img"
+                      height="400"
+                      image="/img/Cafe.png"
+                      alt="Final fantasy x"
+                    />
+                </a>
+                </CardActionArea>
+              </Card>
+            </Grid>
+
+            <Grid item xs={6}>
+              <Card>
+                <a
+                  className="App-link"
+                  href="https://github.com/Zynkah"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  My GitHub
+                </a>
+              </Card>
+            </Grid>
+
+            <Grid item xs={6}>
+              <Card>
+                <a
+                  className="App-link"
+                  href="https://www.linkedin.com/in/zena-creps-ba3631232/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
       </header>
     </div>
   );
