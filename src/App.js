2f19e50f3e1732fbe00e3f61601c568df43d4528
Zynkah <ffgurl07@yahoo.com>
2023-06-07 09:20:20 -0500

    components added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import "./App.css";
 import Footer from "./components/Footer";
 import "@fontsource/roboto/300.css";
 import * as React from "react";
-import { Typography } from "@mui/material";
+import Header from "./components/Header";
 import Cards from "./components/Cards";
 import Bio from "./components/Bio";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Typography variant="h1">Zena Creps</Typography>
-      </header>
+      <Header />
       <Bio />
       <Cards />
       <Footer />
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <Typography variant="h1">Zena Creps</Typography>
+    </header>
+  );
+};
+export default Header;
