c3dd4db68dd9fd4ed924b3a84dee6d0a11952f37
Zynkah <ffgurl07@yahoo.com>
2023-06-14 10:05:08 -0500

    title adjustment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
-import { Typography } from "@mui/material";
+// import { Typography } from "@mui/material";
+import { useSpring, animated } from "@react-spring/web";
+import { useState } from "react";
 
 const Header = () => {
+  const [state, toggle] = useState(true);
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: state ? 1 : 0,
+    config: { duration: 1000 },
+  });
   return (
     <header className="App-header">
-      <Typography variant="h1">Zena Creps</Typography>
+      <div onClick={() => toggle(!state)}>
+        <animated.h1
+          style={{
+            opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+            scale: x.to({
+              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+              output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+            }),
+          }}
+        >
+          Zena Creps
+        </animated.h1>
+      </div>
+      {/* <Typography variant="h1">Zena Creps</Typography> */}
     </header>
   );
 };
